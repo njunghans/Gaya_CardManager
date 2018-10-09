843e84f557b9397f43cff695f960e743e0c2347c
njunghans <lolipopgan320>
2018-10-09 12:13:20 +0200

    Add Cards page

--- resources/views/cards.blade.php
+++ resources/views/cards.blade.php
@@ -70,7 +70,6 @@
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
-                        <a href="{{ route('cards') }}">Cards</a>
                         <a href="{{ route('login') }}">Login</a>
                         <a href="{{ route('register') }}">Register</a>
                     @endauth
