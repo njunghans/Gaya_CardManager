db720a8a464227094e2710166f91787d0e7a680a
Nino Junghans <junghans.nino@dainst.de>
2018-10-09 15:51:07 +0200

    Add auth

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace gaya\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+
+    }
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('home');
+    }
+    public function cards()
+    {
+        return view('cards');
+    }
+}
