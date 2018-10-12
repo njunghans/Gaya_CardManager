f9a8f7c4323284653fd1db545b2f1b46e775a596
Nino Junghans <junghans.nino@dainst.de>
2018-10-12 18:16:46 +0200

    Update navbar style

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -2,18 +2,18 @@
 <!--<div class="links header container">
 
 </div>-->
-<nav class="navbar navbar-laravel navbar-fixed-top" role="navigation">
+<nav class="navbar navbar-expand-md navbar-light navbar-laravel" role="navigation">
     <div class="container">
         <div class="navbar-left">
-            <a class="btn btn-default navbar-btn" href="{{ route('start') }}">Gaya</a>
-            <a class="btn btn-default navbar-btn" href="{{ route('rules') }}">Rules</a>
-            <a class="btn btn-default navbar-btn" href="{{ route('cards.index') }}">Cards</a>
+            <a class="btn btn-default navbar-brand" href="{{ route('start') }}">Gaya</a>
+            <a class="btn btn-default navbar-brand" href="{{ route('rules') }}">Rules</a>
+            <a class="btn btn-default navbar-brand" href="{{ route('cards.index') }}">Cards</a>
         </div>
         @if (Route::has('login'))
             @auth
-                <a class="btn btn-default navbar-btn navbar-right" href="{{ route('logout') }}">Logout</a>
+                <a class="btn btn-default navbar-brand navbar-right" href="{{ route('logout') }}">Logout</a>
             @else
-                <a class="btn btn-default navbar-btn navbar-right" href="{{ route('login') }}">Login</a>
+                <a class="btn btn-default navbar-brand navbar-right" href="{{ route('login') }}">Login</a>
             @endauth
         @endif
     </div>
