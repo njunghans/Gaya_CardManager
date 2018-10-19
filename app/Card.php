6ff200093464df6995b5090869a9754a8cfaf2ca
Nemo Grippa <nemo.grippa@dainst.de>
2018-10-20 01:01:27 +0200

    Remove all IDE Warnings

--- app/Card.php
+++ app/Card.php
@@ -3,6 +3,14 @@
 namespace gaya;
 
 use Illuminate\Database\Eloquent\Model;
+/**
+ * Card
+ *
+ * A Gaya Card
+ *
+ * @mixin \Eloquent
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
 
 class Card extends Model
 {
