de7f6df9a46f7aea1eeefcb565f4fe18046f37bf
Nino Junghans <junghans.nino@dainst.de>
2018-11-03 01:25:42 +0100

    First renderings

--- public/js/render.js
+++ public/js/render.js
@@ -1,30 +1,91 @@
 console.log('hallo');
 
-const app = new PIXI.Application();
 
+const app = new PIXI.Application({width: 630, height:880, transparent:true});
 // The application will create a canvas element for you that you
 // can then insert into the DOM
 document.body.appendChild(app.view);
 
-// load the texture we need
-PIXI.loader.add('bunny', '/data/AttackIcon.png').load((loader, resources) => {
-    // This creates a texture from a 'bunny.png' image
-    const bunny = new PIXI.Sprite(resources.bunny.texture);
+//load an image and run the `setup` function when it's done
+PIXI.loader
+    .add('frame',"/data/Frame.png")
+    .add('rarityStoneCommon','/data/RarityStoneCommon.png')
+    .load(setup);
+
+let style = new PIXI.TextStyle({
+    fontFamily: "Arial",
+    fontSize: 36,
+    fill: "white",
+    stroke: 'black',
+    strokeThickness: 4,
+    dropShadow: true,
+    dropShadowColor: "#000000",
+    dropShadowBlur: 4,
+    dropShadowAngle: Math.PI / 6,
+    dropShadowDistance: 6,
+});
+
+let title = new PIXI.Text("Test Card", style);
+
+title.x = 180;
+title.y = 70;
+
+//This `setup` function will run when the image has loaded
+function setup() {
+
+    //Create the cat sprite
+    let frame = new PIXI.Sprite(PIXI.loader.resources.frame.texture);
+    frame.width=630;
+    frame.height=880;
 
     // Setup the position of the bunny
-    bunny.x = app.renderer.width / 2;
-    bunny.y = app.renderer.height / 2;
+    frame.x = 0;
+    frame.y = 0;
 
-    // Rotate around the center
-    bunny.anchor.x = 0.5;
-    bunny.anchor.y = 0.5;
+    let rarityStoneCommon = new PIXI.Sprite(PIXI.loader.resources.rarityStoneCommon.texture);
+
+    rarityStoneCommon.width=80;
+    rarityStoneCommon.height=70;
+    rarityStoneCommon.x = 75;
+    rarityStoneCommon.y = 540;
+
+    //Add the cat to the stage
+    app.stage.addChild(frame);
+    app.stage.addChild(rarityStoneCommon);
+
+    app.stage.addChild(title);
+}
+/*
+// load the texture we need
+PIXI.loader.add('bunny', '/data/Frame.png').load((loader, resources) => {
+    // This creates a texture from a 'bunny.png' image
+    const bunny = new PIXI.Sprite(resources.bunny.texture);
+    bunny.width=630;
+    bunny.height=880;
+
+    // Setup the position of the bunny
+    bunny.x = 0;
+    bunny.y = 0;
+
+    // Add the bunny to the scene we are building
+    app.stage.addChild(bunny);
+    app.stage.addChild(bunny);
+
+});
+
+
+PIXI.loader.add('bunny', '/data/RarityStoneCommon.png').load((loader, resources) => {
+    // This creates a texture from a 'bunny.png' image
+    const bunny = new PIXI.Sprite(resources.bunny.texture);
+    bunny.width=190;
+    bunny.height=170;
+
+    // Setup the position of the bunny
+    bunny.x = 100;
+    bunny.y = 400;
 
     // Add the bunny to the scene we are building
     app.stage.addChild(bunny);
 
-    // Listen for frame updates
-    app.ticker.add(() => {
-        // each frame we spin the bunny around a bit
-        bunny.rotation += 0.01;
-    });
 });
+*/
