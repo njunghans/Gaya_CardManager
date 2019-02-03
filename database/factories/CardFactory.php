43dac1f116571c40de29365e1a5c128f658fd8c9
Nemo Grippa <nemo.grippa@dainst.de>
2019-02-03 15:03:58 +0100

    Improve cards seeder

--- database/factories/CardFactory.php
+++ database/factories/CardFactory.php
@@ -7,24 +7,39 @@ function rand_entry($key) {
     return array_rand(config("gaya.drop_downs.$key"));
 }
 
+function rand_cost() {
+    $r = rand(0, 100);
+    if ($r < 30) {
+        $c = 0;
+    } elseif (30 <= $r && $r< 55) {
+        $c = 1;
+    } elseif (55 <= $r && $r < 75) {
+        $c = 2;
+    } elseif (75 <= $r && $r < 90) {
+        $c = 3;
+    } else {
+         $c = rand(4, 10);
+     }
+     return $c;
+}
 
 $factory->define(gaya\Card::class, function (Faker $faker) {
     return [
         'user_id' => rand(1, config('seeding.numbers.default')),
         'name' => $faker->name,
         'card_type' => rand_entry('card_types'),
-        'category_text' => $faker->text,
+        'category_text' => str_random(8),
         'edition_id' => rand(1, config('seeding.numbers.default')),
-        'cost_gold' => rand(0, 10),
-        'cost_neutral' => rand(0, 10),
-        'cost_bio' => rand(0, 10),
-        'cost_ene' => rand(0, 10),
-        'cost_par' => rand(0, 10),
-        'cost_art' => rand(0, 10),
-        'attack' => rand(0, 10),
-        'life' => rand(0, 10),
-        'shield' => rand(0, 10),
-        'range' => rand(0, 10),
+        'cost_gold' => rand_cost(),
+        'cost_neutral' => rand_cost(),
+        'cost_bio' => rand_cost(),
+        'cost_ene' => rand_cost(),
+        'cost_par' => rand_cost(),
+        'cost_art' => rand_cost(),
+        'attack' => rand_cost(),
+        'life' => rand_cost(),
+        'shield' => rand_cost(),
+        'range' => rand_cost(),
         'card_effect' => $faker->text,
         'lore_text' => $faker->text,
         'official' => false,
