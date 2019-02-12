7c9a16efea6f5b0acdf93344a1ad72a81e75e709
Nemo Grippa <nemo.grippa@dainst.de>
2019-02-12 22:31:08 +0100

    Add Link to Profile

--- resources/views/layouts/nav/top-right.blade.php
+++ resources/views/layouts/nav/top-right.blade.php
@@ -9,11 +9,13 @@
     @endif
 @else
     <li class="nav-item dropdown">
-        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+           aria-haspopup="true" aria-expanded="false" v-pre>
             {{ Auth::user()->username }} <span class="caret"></span>
         </a>
 
         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="{{route('home')}}">Profile</a>
             <a class="dropdown-item" href="{{ route('logout') }}"
                onclick="event.preventDefault();
                                                          document.getElementById('logout-form').submit();">
